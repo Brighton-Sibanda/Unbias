24e711a91bb754ed00aec3356741e3a64a51bcf1
Desmond Nebah <nebahdesmondfongwa@gmial.com>
2024-10-05 14:04:35 -0500

    clerk user account management + landing page

--- unbias/src/App.js
+++ unbias/src/App.js
@@ -7,18 +7,45 @@ import Home from './Home';
 import Login from './Login';
 import MyFooter from "./Footer";
 import "./index.css";
+import { ClerkProvider } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+import './Login.css';
+
+// Clerk publishable key
+const PUBLISHABLE_KEY = "pk_test_a2Vlbi1tYXJsaW4tMzQuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <MyNavbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
-        </Routes>
-        <MyFooter />
-      </BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <header>
+
+      {/* Display welcome page here when user clicks on Unbias.com */}
+      <SignedOut>  
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+      </SignedOut>
+
+      {/* The user will be taken to the main page after they sign-in */}
+      <SignedIn>
+        <UserButton />
+        <BrowserRouter>
+          <MyNavbar />
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+          <MyFooter />
+        </BrowserRouter>
+      </SignedIn>
+      </header>
+      </ClerkProvider>
     </div>
   );
 }
--- unbias/src/Login.js
+++ unbias/src/Login.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import { SignInButton } from '@clerk/clerk-react';
+import './Login.css';
 
-function Footer() {
+function Login() {
     return (
-        <div className="Login">
-            <p> Login Page Here </p>
-            <a href="/home"> <button>Click to bypass login</button></a>
+        <div className="login-wrapper">
+            <div className="login-content">
+                <h1>Welcome to Unbias</h1>
+                <p>Getting diverse, expert advice on your most critical decisions has never been simpler</p>
+                <div className="login-button">
+                    <SignInButton />
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Footer;
+export default Login;
