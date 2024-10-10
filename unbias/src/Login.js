53ca03c832dcdd606bb0ed91edde0d3de0f50a07
Desmond Nebah <nebahdesmondfongwa@gmial.com>
2024-10-10 09:52:23 -0500

    css change

--- unbias/src/Login.js
+++ unbias/src/Login.js
@@ -6,7 +6,7 @@ function Login() {
     return (
         <div className="login-wrapper">
             <div className="login-content">
-                <h1>Welcome to Unbias</h1>
+                <h1 className='login-welcome'>Welcome to Unbias</h1>
                 <p>Getting diverse, expert advice on your most critical decisions has never been simpler</p>
                 <div className="login-button">
                     <SignInButton />
